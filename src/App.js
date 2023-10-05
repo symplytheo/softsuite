fe43b876b005f640cb4fb49aeb2da30ab2ea6bc7
Theo Ojochegbe <symplytheo@gmail.com>
2023-10-05 21:35:29 +0100

    feat: implement basic routing

--- src/App.js
+++ src/App.js
@@ -1,21 +1,36 @@
 import AppFooter from "./components/footer";
 import AppHeader from "./components/header";
 import Sidebar from "./components/sidebar";
-import ElemetsPage from "./views/elements";
+import { Outlet, RouterProvider, createBrowserRouter } from "react-router-dom";
+import ElementsPage from "./views/elements";
+import ElementLinksPage from "./views/element-link";
+
+const Layout = () => (
+  <div className="App">
+    <Sidebar />
+    <div>
+      <AppHeader />
+      <main>
+        <Outlet />
+      </main>
+    </div>
+    <AppFooter />
+  </div>
+);
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      { path: "", element: <ElementsPage /> },
+      { path: "/:elementId", element: <ElementLinksPage /> },
+    ],
+  },
+]);
 
 function App() {
-  return (
-    <div className="App">
-      <Sidebar />
-      <div>
-        <AppHeader />
-        <main>
-          <ElemetsPage />
-        </main>
-      </div>
-      <AppFooter />
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
