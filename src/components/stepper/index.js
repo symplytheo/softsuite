6730846495170bf2a7cc5842bb78ceb1025f5766
Theo Ojochegbe <symplytheo@gmail.com>
2023-10-05 12:31:50 +0100

    feat: create stepper component

---
+++ src/components/stepper/index.js
@@ -0,0 +1,40 @@
+import React from "react";
+import s from "./stepper.module.scss";
+
+const Stepper = ({ steps = [], activeStep = 0, showIndicators = true }) => {
+  // indicator
+  const StepperIndicator = () => (
+    <div className={s.stepper_indicators}>
+      {steps.map((item, index) => (
+        <div
+          className={[s.step, activeStep > index ? s.done : activeStep === index ? s.active : s.inactive].join(" ")}
+          key={index}
+        >
+          <div className={s.label}>{item.label}</div>
+          <div>
+            <div className={s.circle}>{index < activeStep ? <span>&#10003;</span> : index + 1}</div>
+          </div>
+          {index < steps.length - 1 && <div className={s.line}></div>}
+        </div>
+      ))}
+    </div>
+  );
+
+  return (
+    <div className={s.stepper}>
+      {showIndicators && <StepperIndicator steps={steps} activeStep={activeStep} />}
+      {/* content */}
+      <div className={s.stepper_content}>
+        {steps.map((item, index) =>
+          activeStep === index ? (
+            <div className={s.step}>
+              <item.component />
+            </div>
+          ) : null
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Stepper;
