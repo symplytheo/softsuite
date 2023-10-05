027693969c076f8018fc0b2b363c9aba72a5e9f4
Theo Ojochegbe <symplytheo@gmail.com>
2023-10-05 12:32:03 +0100

    chore: clean up code, use stepper component

--- src/views/elements/create-element-modal/index.js
+++ src/views/elements/create-element-modal/index.js
@@ -1,7 +1,8 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Modal from "../../../components/modal";
 import StepTwo from "./steptwo";
 import StepOne from "./stepone";
+import Stepper from "../../../components/stepper";
 import s from "../elements.module.scss";
 
 const CreateElementModal = ({ isOpen, onClose }) => {
@@ -15,23 +16,21 @@ const CreateElementModal = ({ isOpen, onClose }) => {
     setActiveStep((prevStep) => prevStep - 1);
   };
 
+  useEffect(() => {
+    if (!isOpen) {
+      setActiveStep(0);
+    }
+  }, [isOpen]);
+
+  const STEPS = [
+    { label: "Element Details", component: () => <StepOne handleNext={handleNext} onClose={onClose} /> },
+    { label: "Additional Details", component: () => <StepTwo handlePrev={handlePrev} onClose={onClose} /> },
+  ];
+
   return (
     <Modal isOpen={isOpen} onClose={onClose} maxWidth={780}>
       <h3 className={s.modal_title}>Create Element</h3>
-      <div className={s.stepper}>
-        <div className={s.stepper_content}>
-          {activeStep === 0 && (
-            <div className={s.step}>
-              <StepOne handleNext={handleNext} onClose={onClose} />
-            </div>
-          )}
-          {activeStep === 1 && (
-            <div className={s.step}>
-              <StepTwo handlePrev={handlePrev} onClose={onClose} />
-            </div>
-          )}
-        </div>
-      </div>
+      <Stepper steps={STEPS} activeStep={activeStep} />
     </Modal>
   );
 };
