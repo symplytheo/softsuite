98ef1d8ca0bf59df8f358e3e494cc6c1723bb2e4
Theo Ojochegbe <symplytheo@gmail.com>
2023-10-06 18:35:49 +0100

    fix: return value changes

--- src/components/input/index.js
+++ src/components/input/index.js
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import s from "./input.module.scss";
 
-const Input = ({ error, hint, ...rest }) => {
+const Input = ({ error, hint, value, onChange = () => {}, ...rest }) => {
+  const [text, setText] = useState(value);
+  const handleChange = (val) => {
+    setText(val);
+    onChange(val);
+  };
+
   return (
     <div className={s.container}>
-      <input className={`${s.input} ${error ? s.error : ""}`} {...rest} />
+      <input
+        value={text}
+        onChange={(e) => handleChange(e.target.value)}
+        className={`${s.input} ${error ? s.error : ""}`}
+        {...rest}
+      />
       {error && <div className={s.errormessage}>{error}</div>}
       {!error && hint && <div className={s.hint}>{hint}</div>}
     </div>
--- src/components/textarea/index.js
+++ src/components/textarea/index.js
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import s from "./textarea.module.scss";
 
-const TextArea = ({ error, hint, ...rest }) => {
+const TextArea = ({ error, hint, value, onChange = () => {}, ...rest }) => {
+  const [text, setText] = useState(value);
+  const handleChange = (val) => {
+    setText(val);
+    onChange(val);
+  };
+
   return (
     <div className={s.container}>
-      <textarea className={`${s.input} ${error ? s.error : ""}`} {...rest} />
+      <textarea
+        value={text}
+        onChange={(e) => handleChange(e.target.value)}
+        className={`${s.input} ${error ? s.error : ""}`}
+        {...rest}
+      />
       {error && <div className={s.errormessage}>{error}</div>}
       {!error && hint && <div className={s.hint}>{hint}</div>}
     </div>
